532e96e10fffacbf31670bb69ad0d1c1d1af7a2a
Rich Lott / Artful Robot <forums@artfulrobot.uk>
2021-07-14 12:37:34 +0100

    import

--- README.md
+++ README.md
@@ -16,3 +16,12 @@ giving access to the rows referenced by column name, column/row indexes.
 See the Synopsis section in the docblock of `ArtfulRobot/CSVParser.php`
 
 
+## Change log
+
+- v1.0 This began life bundled with the artfulrobot/artfulrobot PHP
+  libraries, but was separated out as a lot of the other code in that
+  library is deprecated. So it's been in production use for years. If you
+  previously depended on artfulrobot/artfulrobot for the CSV libraries you
+  should update to artfulrobot/csv which is where future maintenance will
+  happen. In this case you should note that the class names have been
+  standardised, so it's now `CSVParser` not `CsvParser`.
---
+++ tests/CSVParserTest.php
@@ -0,0 +1,65 @@
+<?php
+/*
++--------------------------------------------------------------------+
+| Copyright Rich Lott 2021. All rights reserved.                     |
+|                                                                    |
+| This work is published under the GNU GPLv3.0 license with some     |
+| permitted exceptions and without any warranty. For full license    |
+| and copyright information, see LICENSE                             |
++--------------------------------------------------------------------+
+*/
+use \ArtfulRobot\CSVParser;
+
+class CSVParserTest extends \PHPUnit\Framework\TestCase {
+
+    public function testBasicParse()
+    {
+      $csv = CSVParser::createFromFile(dirname(__FILE__) . '/fixtures/testcase-1.csv');
+      $i=0;
+      $expectations = [['Rich',  40], ['Fred',  1000], ['Wilma',  0], ['',  56], ['Bam Bam', '']];
+      foreach ($csv as $row) {
+        $expect = array_shift($expectations);
+        $this->assertEquals($expect[0], $row->Name);
+        $this->assertEquals($expect[1], $row->Age);
+        $i++;
+      }
+      $this->assertEquals(5, $i, "Expected 4 records, got $i");
+    }
+
+
+    public function testRandomAccess()
+    {
+      $csv = new CSVParser();
+      $csv->loadFromFile(dirname(__FILE__) . '/fixtures/testcase-1.csv');
+      $this->assertEquals('Rich', $csv->Name);
+      $this->assertEquals('Rich', $csv->getCell(0));
+      $this->assertEquals('Rich', $csv->getCell(0,1));
+      $this->assertEquals('Fred', $csv->getCell(0,2));
+    }
+    public function testMultiLine()
+    {
+      $csv = new CSVParser();
+      $csv->loadFromFile(dirname(__FILE__) . '/fixtures/testcase-2.csv');
+      $i=0;
+      $expectations = [['Fred',  "The Cave\nHovelshire"], ['Betty',  'Elsewhere']];
+      foreach ($csv as $row) {
+        $expect = array_shift($expectations);
+        $this->assertEquals($expect[0], $row->Name);
+        $this->assertEquals($expect[1], $row->Address);
+        $i++;
+      }
+      $this->assertEquals(2, $i, "Expected 2 records, got $i");
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Duplicate header name: Name
+     */
+    public function testDodgyHeaders()
+    {
+      $csv = new CSVParser();
+      $csv->loadFromFile(dirname(__FILE__) . '/fixtures/testcase-3-dodgy-headers.csv');
+    }
+}
+
+
